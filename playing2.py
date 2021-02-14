1083981d40efb8a69d6e583f5574a2998da79385
bert386 <eastecdev2011@gmail.com>
2021-02-14 00:08:24 -0800

    Playing state handler for track 02

---
+++ playing2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""   
+Description:
+
+Todo:
+
+"""
+
+import os
+import sys
+import logging
+from collections import deque
+from base_state import BaseState
+
+
+class Playing2ndState(BaseState):
+    """"""
+
+    def __init__(self, state_controller):
+        super().__init__(state_controller, self.in_state)
+
+    @BaseState.decorator_enter
+    def on_entered(self):
+        logging.warning("Track02 started ...")
+
+    def in_state(self):
+        pass
+
+    @BaseState.decorator_exit
+    def on_exited(self):
+        pass
